72f2b924fd7ac406308eda2ccd7d1da76fa85f08
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-23 21:43:06 +0530

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const app = express();
 
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
