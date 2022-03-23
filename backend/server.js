b49cfc7495482ea3c4057a8d97db312972d80481
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-23 19:25:28 +0530

    fetch data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 const app = express();
 
 dotenv.config();
@@ -12,14 +12,8 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
