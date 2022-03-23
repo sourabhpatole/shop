63f032d0bd9e967571dc3f3a063b63c6632104ca
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-23 15:24:02 +0530

    es module in node

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 const app = express();
-const products = require("./data/products");
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -13,5 +16,8 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
-
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT} `)
+);
