21d914ab3e78a7cc5d0d73830e1577fa5af4d28e
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-24 14:47:10 +0530

    user auth endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 const userSchema = mongoose.Schema(
   {
@@ -22,5 +23,8 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model("User", userSchema);
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
