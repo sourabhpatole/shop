8eeb34044b1e0cf0e1f749da61db941fc213e400
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-24 19:14:36 +0530

    user sign in

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import {
-    USER_LOGIN_FAIL,
+  USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -36,4 +37,8 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
