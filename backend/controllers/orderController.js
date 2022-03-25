0c75c73b598d4ce50ddf39ee6e1145761ea21688
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-25 18:23:39 +0530

    order details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,4 +37,20 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc    Get order by Id
+// @route   GET /api/orders
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Not found Order");
+  }
+});
+
+export { addOrderItems, getOrderById };
