897d24aa4a77378fa6bd6b0fe3f7d5de18e0e6d2
sourabhpatole <sourabhptole@outlook.com>
2022-03-28 13:42:18 +0530

    admin page user list

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -25,4 +25,13 @@ const protect = asyncHandler(async (req, res, next) => {
     throw new Error("Not authorized, no Token");
   }
 });
-export { protect };
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as admin");
+  }
+};
+export { protect, admin };
